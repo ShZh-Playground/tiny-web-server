2c598b589a2c2fac5905df5eeadf27cd1d67d944
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-07-22 23:22:50 +0800

    feat: build basic web server logic flow.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+
+fn handle_stream(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").unwrap();
+
+    for stream in listener.incoming() {
+        handle_stream(stream.unwrap());
+    }
 }
