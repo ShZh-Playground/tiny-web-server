b710bba776637c51631b5c8a19565bbe493d67cf
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-07-23 07:56:44 +0800

    feat: support return html in response.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::fs::File;
 
 fn handle_stream(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("{}", String::from_utf8_lossy(&buffer));
+    
+    let mut html_template = File::open("./resource/hello.html").unwrap();
+    let mut content = String::new();
+    html_template.read_to_string(&mut content).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}\r\n", content);
+    println!("{}", response);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn main() {
