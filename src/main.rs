6265e1180a6d3ec8f840582117bfe7d989283947
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-07-23 08:26:46 +0800

    feat: handle 404 scenario.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@ use std::io::prelude::*;
 use std::fs::File;
 
 fn handle_stream(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
+    let mut request_buffer = [0; 512];
+    stream.read(&mut request_buffer).unwrap();
+
+    let (resource_location, response_header) = if request_buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+        ("./resource/hello.html", "HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+        ("./resource/404.html", "HTTP/1.1 404 NOT FOUND\r\n\r\n")
+    };
     
-    let mut html_template = File::open("./resource/hello.html").unwrap();
+    let mut html_template = File::open(resource_location).unwrap();
     let mut content = String::new();
     html_template.read_to_string(&mut content).unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
-    println!("{}", response);
+    let response = format!("{}{}", response_header, content);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
